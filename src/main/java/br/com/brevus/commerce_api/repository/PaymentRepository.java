c1aa22d5956b3ed1d9989b0dff44256754e67855
Gabriel Cabral <gabrielcabral@MacBook-Pro-de-Gabriel.local>
2026-03-15 12:30:46 -0300

    home manager e tela faturamento

--- src/main/java/br/com/brevus/commerce_api/repository/PaymentRepository.java
+++ src/main/java/br/com/brevus/commerce_api/repository/PaymentRepository.java
@@ -1,11 +1,31 @@
 package br.com.brevus.commerce_api.repository;
 
+import br.com.brevus.commerce_api.enums.PaymentStatus;
 import br.com.brevus.commerce_api.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 
     boolean existsBySaleId(UUID saleId);
+
+    List<Payment> findTop10ByStatusOrderByCreatedAtDesc(PaymentStatus status);
+
+    @Query("""
+    SELECT COALESCE(SUM(p.amount), 0)
+    FROM Payment p
+    WHERE p.status = :status
+    AND p.createdAt BETWEEN :startDate AND :endDate
+""")
+    BigDecimal sumPaymentsByStatusAndPeriod(
+            @Param("status") PaymentStatus status,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate
+    );
 }
